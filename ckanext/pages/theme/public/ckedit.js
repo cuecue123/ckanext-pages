2f6e07b0a0326d3b5c5ac5a2881c42952c6e9356
Jay Guo <jayguo@Jays-MBP.lan>
2017-05-16 11:31:55 -0400

    Added ckedit plugins for templating and editing source html

--- ckanext/pages/theme/public/ckedit.js
+++ ckanext/pages/theme/public/ckedit.js
@@ -18,27 +18,28 @@ this.ckan.module('ckedit', function (jQuery, _) {
         { name: 'insert' },
         { name: 'forms' },
         { name: 'tools' },
-        { name: 'document',	   groups: [ 'document', 'doctools' ] },
+        { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
         { name: 'others' },
         '/',
         { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-        { name: 'paragraph',   groups: [ 'list', 'blocks', 'align', 'bidi' ] },
+        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
         { name: 'styles' },
       ];
 
       // Remove some buttons, provided by the standard plugins, which we don't
       // need to have in the Standard(s) toolbar.
-      config.removeButtons = 'Underline,Subscript,Superscript';
+      //config.removeButtons = 'Underline,Subscript,Superscript';
 
       // Se the most common block elements.
       config.format_tags = 'p;h1;h2;h3;pre';
 
       // Make dialogs simpler.
-      config.removeDialogTabs = 'image:advanced;link:advanced';
+      //config.removeDialogTabs = 'image:advanced;link:advanced';
       config.filebrowserUploadUrl = this.options.site_url + 'pages_upload';
-      config.extraPlugins = 'divarea,ckanview';
+      config.extraPlugins = 'divarea,ckanview,smiley,templates';
       config.height = '400px';
       config.customConfig = false;
+      config.allowedContent = true;
 
       var editor = $(this.el).ckeditor(config);
     },
